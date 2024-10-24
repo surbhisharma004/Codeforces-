7806be1d39d4ebe4c22379aa55202ab0d30f2e19
surbhi sharma <142304352+surbhisharma004@users.noreply.github.com>
2024-10-24 20:11:46 +0530

    Create 1030A.cpp

---
+++ 1030A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n,s;
+cin>>n;
+for(int i=0;i<n;i++){
+cin>>s;
+if(s==1){
+    cout<<"hard";
+    return 0;
+}}
+cout<<"easy";
+return 0;
+}
+
+
