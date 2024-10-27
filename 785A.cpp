b66adb67c0d62ecc6961e032f38d0c450eadf48a
surbhi sharma <142304352+surbhisharma004@users.noreply.github.com>
2024-10-27 23:46:47 +0530

    Create 785A.cpp

---
+++ 785A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main(){
+    unordered_map<string, int> mp;
+    mp["Tetrahedron"] = 4;
+    mp["Cube"] = 6;
+    mp["Octahedron"] = 8;
+    mp["Dodecahedron"] = 12;
+    mp["Icosahedron"] = 20;
+
+    int n;
+    cin >> n;
+    int faces = 0;
+    for(int i=0;i<n;i++){
+        string str;
+        cin >> str;
+        if(mp.find(str) != mp.end()){
+            faces += mp[str];
+        }
+    }
+
+    cout << faces ;
+    return 0;
+}
