f6a7ad186d7da63cc852e5d4811f71816100f5dd
surbhi sharma <142304352+surbhisharma004@users.noreply.github.com>
2024-11-01 23:17:28 +0530

    Create 1903A.cpp

---
+++ 1903A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool sorted(vector<int>& arr){
+    int n = arr.size();
+    for(int i=1;i<n;i++){
+        if(arr[i] < arr[i-1]) return false;
+    }
+    return true;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> arr(n);
+        for(int i=0;i<n;i++) cin >> arr[i];
+
+        if(k == 1 && !sorted(arr)) cout << "NO" ;
+        else cout << "YES" ;    
+
+    }
+
+    return 0;
+}
