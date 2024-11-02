40b34f7523ed5501e2406cca54a03251d5649309
surbhi sharma <142304352+surbhisharma004@users.noreply.github.com>
2024-11-02 23:46:38 +0530

    Create 34B.cpp

---
+++ 34B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin >> arr[i];
+
+    sort(arr.begin(), arr.end());
+    int result = 0;
+    for(int i=0;i<m;i++){
+        if(arr[i] <= 0) result += -1 * arr[i];
+        else break;
+    }
+
+    cout << result;
+    return 0;
+}
