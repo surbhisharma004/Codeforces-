1997ff8403fa231defec397befcdeb400b01a00f
surbhi sharma <142304352+surbhisharma004@users.noreply.github.com>
2024-11-03 23:58:18 +0530

    Create 136A.cpp

---
+++ 136A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, p;
+    cin >> n;
+    vector<int> arr(n+1);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> p;
+        arr[p] = i;
+    }
+    cout << arr[1];
+    for (int i = 2; i <= n; ++i)
+    {
+        cout << " " << arr[i];
+    }
+   
+    return 0;
+}
