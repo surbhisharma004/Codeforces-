d1934d987c5798484d1e9e9d82c91bc62474bd8f
surbhi sharma <142304352+surbhisharma004@users.noreply.github.com>
2024-11-04 23:50:10 +0530

    Create 32A.cpp

---
+++ 32A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n, d;
+    cin >> n >> d;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+
+    int num1 = 0, num2 = 1, ways = 0;
+    while(num2 < n){
+        if(arr[num2] - arr[num1] <= d){
+            ways += (num2 - num1);
+            num2++;
+        }else num1++;
+    }
+
+    ways *= 2;
+    cout << ways ;
+    return 0;
+}
