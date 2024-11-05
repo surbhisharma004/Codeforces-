fb42675651baa6dde003abdb26518c017a27e72e
surbhi sharma <142304352+surbhisharma004@users.noreply.github.com>
2024-11-05 23:55:23 +0530

    Create 96A.cpp

---
+++ 96A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main(){
+	string s;
+	cin >> s;
+
+	int zero = 0;
+	int one = 0;
+	string result = "NO";
+	for(int i =0; i<s.size(); i++){
+		if(s[i] == '0'){
+			one = 0;
+			zero++;
+		}
+		if(s[i] == '1'){
+			one++;
+			zero = 0;
+		}
+
+		if(zero == 7 || one == 7) {
+            result = "YES"; 
+            break ;
+        }
+
+	}
+
+	cout << result ;
+	return 0;
+}
